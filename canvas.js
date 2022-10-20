5c1aaca712b211e4ee68eb6516124e464a1b4c36
NandoPla <fernandopla8@gmail.com>
2022-10-20 00:57:47 -0300

    canvas training

--- canvas.js
+++ canvas.js
@@ -13,15 +13,54 @@ window.addEventListener("load", () => {
    context.fillRect(50, 50, 250, 250);
    context.strokeRect(400, 50, 250, 250);
 
+   // iniciar o traço:
    context.beginPath();
+
    // (eixo x, eixo y, esticando)
    context.moveTo(820,300);
-   // (eixo x, qt +, + afasta / eixo y, qt +, + afasta, com a ponta do inicio do desenho)
-   context.lineTo(900,50);
+
+   // (eixo x, qt +, + afasta / eixo y, qt +, + afasta, com a ponta do inicio do desenho):
+   context.lineTo(910,50);
+
+   //Define a cor:
    context.strokeStyle = "blue";
+
    context.lineTo(1000,300);
-   context.lineTo(818,300);
+   
+   // fecha os pontos:
+   context.closePath();
    context.stroke();
+
+   //variaveis
+
+   let painting = false;
+
+   function posicaoInicial(e){
+    painting = true;
+    draw(e);
+   }
+
+   function posicaoFinal(){
+    painting = false;
+    context.beginPath();
+   }
+
+   function draw (e){
+    if(!painting) return;
+    context.lineWidth = 20;
+    context.lineCap = "round";
+
+    context.lineTo(e.clientX, e.clientY);
+    context.stroke();
+    context.beginPath();
+    context.moveTo(e.clientX, e.clientY);
+   }
+   //EventListeners
+
+   canvas.addEventListener("mousedown", posicaoInicial);
+   canvas.addEventListener("mouseup", posicaoFinal);
+   canvas.addEventListener("mousemove", draw);
+
    
 });
 
